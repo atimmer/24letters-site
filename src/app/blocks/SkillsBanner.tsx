87fa884a1152d92fdf059eda0147a7e08a281e81
Anton Timmermans <email@atimmer.com>
2024-11-04 13:21:17 +0100

    Remove vertical scrollbar from skills banner
    Because the class was not specifying the direction of the overflow,
    there would be scrollbars in both cardinal directions. Instead,
    specify to only show scrollbars in the horizontal direction. Also,
    make sure that when the screen is wide enough we don't show any
    scrollbars.

--- src/app/blocks/SkillsBanner.tsx
+++ src/app/blocks/SkillsBanner.tsx
@@ -13,12 +13,10 @@ const skills = [
 
 export default function SkillsBanner() {
   return (
-    <aside className="bg-dark relative overflow-scroll px-4 pr-8 font-heading text-3xl text-white lg:px-0">
+    <aside className="bg-dark relative overflow-x-scroll px-4 pr-8 font-heading text-3xl text-white lg:overflow-x-auto lg:px-0">
       <Container className="flex justify-between gap-6 text-nowrap py-6">
         {skills.map((skill) => (
-          <span className="" key={skill}>
-            {skill}
-          </span>
+          <span key={skill}>{skill}</span>
         ))}
       </Container>
     </aside>
