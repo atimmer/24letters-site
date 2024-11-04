f17f9c1b7c8d9af263266a83b70ed8dc19fe0c6c
Anton Timmermans <email@atimmer.com>
2024-11-04 13:00:55 +0100

    Extract headshot header to separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,10 @@
 import Image from "next/image";
-import Container from "@/primitives/Container";
-import Headshot from "@/images/Headshot.png";
+import { HeadshotHeader } from "@/app/blocks/HeadshotHeader";
 
 export default function Home() {
   return (
     <>
-      <header className="bg-cyprus">
-        <Container className="flex flex-col items-center gap-4 px-6 pb-6 font-heading text-white sm:grid sm:grid-cols-2 lg:gap-20 lg:pb-10">
-          <div className="flex w-full max-w-sm flex-col justify-center md:max-w-none">
-            <div>
-              <span className="text-2xl md:text-3xl lg:text-[5rem]">I’m </span>
-              <span className="text-5xl md:text-6xl lg:text-9xl lg:leading-tight">
-                Anton,
-              </span>
-            </div>
-            <div className="md:text-xl lg:text-6xl lg:leading-tight">
-              Creative full-stack software engineer
-            </div>
-          </div>
-          <aside className="max-w-sm md:max-w-none">
-            <Image
-              src={Headshot}
-              alt="Avatar of Anton Timmermans"
-              width={640}
-              height={640}
-              priority
-            />
-          </aside>
-        </Container>
-      </header>
+      <HeadshotHeader />
       <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
         <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
           <Image
