f90ea05bb7f1798317ba9af1e80960b0f3864c3a
Anton Timmermans <email@atimmer.com>
2024-11-05 12:02:27 +0100

    Improve bio with written text

--- src/app/blocks/Bio.tsx
+++ src/app/blocks/Bio.tsx
@@ -6,24 +6,41 @@ export default function Bio() {
     <BaseBlock className="max-w-[800px] md:mx-auto">
       <BaseBlockHeading>About</BaseBlockHeading>
       <p className="mt-6">
-        Creating software that solves real world problems is what I do.
-        Communicating about those solutions is what I love. Humans are more
-        important than software.
+        As an entrepreur I understand that Software Engineering is not about
+        building the shiniest solutions. What really matters is solving the
+        problem at hand. I can help you solve problems and create software that
+        makes the impact you are looking for.
+      </p>
+      <p className="mt-4">
+        Over the years of my career I have learned that every situation requires
+        a different approach. The tools that we use are just that, tools. That’s
+        why I’ve familiarized myself with a broad range of different kinds of
+        tools. React is sometimes the right tool for the job. WordPress is
+        sometimes the right tool for the job. X is sometimes the right tool…
       </p>
       <p className="mt-4">
         It is my belief that people’s lives can be improved tremendously by
         creating the right software.
       </p>
       <p className="mt-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        <ul>
+          <li>
+            <strong>Phone</strong>: +31 6 11920738
+          </li>
+          <li>
+            <strong>Email</strong>: anton@24letters.com
+          </li>
+          <li>
+            <strong>Website</strong>: 24letters.com
+          </li>
+          <li>
+            <strong>LinkedIn</strong>: https://www.linkedin.com/in/atimmer/
+          </li>
+          <li>
+            <strong>KVK</strong>: 89299868
+          </li>
+        </ul>
       </p>
     </BaseBlock>
   );
-  return <section className="py-10">Bio</section>;
 }
