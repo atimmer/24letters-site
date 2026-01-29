5b0f2d50acee939f86a627ef8e520149c0712f4b
Anton Timmermans <email@atimmer.com>
2026-01-29 16:17:37 +0100

    feat: refine testimonial links

--- src/app/blocks/Testimonials.tsx
+++ src/app/blocks/Testimonials.tsx
@@ -4,15 +4,41 @@ import Container from "@/primitives/Container";
 import Omar from "@/images/Omar.jpeg";
 import Niels from "@/images/Niels.jpeg";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Testimonials() {
+  const rotatingTestimonials = [
+    Math.random() < 0.5
+      ? {
+          name: "Omar Reiss",
+          title: "CTO at Yoast",
+          image: Omar,
+          alt: "Photo of Omar Reiss",
+          linkedin: "https://www.linkedin.com/in/omarreiss/",
+          quote:
+            "Anton is a multi-talent, a really smart, enthusiastic and curious person. He is truly full stack as a software engineer and a very fast learner. He’s also proven himself to be a great mentor and teacher to his fellow engineers. I’ve deeply enjoyed working with him and have learned a lot in doing so. Don’t miss out on a chance to work with him!",
+        }
+      : {
+          name: "Niels de Blauw",
+          title: "Lead Developer at Level Level",
+          image: Niels,
+          alt: "Photo of Niels de Blauw",
+          linkedin: "https://www.linkedin.com/in/nielsdeblaauw/",
+          quote:
+            "Anton is able to solve very complex problems with creative yet sensible solutions. He picked up multiple roles from building the user interface, adding pricing algorithms and integrating these new features into the production process of the manufacturer. He has extensive knowledge of a lot of aspects in web development. He knows how to clearly communicate his thoughts and when to ask questions to learn more.",
+        },
+  ];
+
   return (
     <Container>
-      <BaseBlock className="bg-gray-100">
-        <BaseBlockHeading className="max-w-[800px] md:mx-auto">
+      <BaseBlock className="bg-gray-100 py-8 md:py-10 lg:py-14">
+        <BaseBlockHeading className="max-w-[800px] xl:px-6">
           Testimonials
         </BaseBlockHeading>
-        <div className="mt-6 md:grid md:grid-cols-2 md:gap-8 xl:px-6">
+        <p className="mt-2 max-w-[800px] text-sm text-gray-600 xl:px-6">
+          Titles reflect roles at the time we worked together.
+        </p>
+        <div className="mt-8 md:grid md:grid-cols-2 md:gap-8 xl:px-6">
           <div className="flex flex-col gap-4">
             <div className="flex items-center gap-6">
               <div
@@ -26,8 +52,18 @@ export default function Testimonials() {
                 <span className="font-heading text-3xl">Craig Smith</span>
                 <br />
                 <span className="text-xl font-light">
-                  Senior Full Stack Engineer
+                  Senior Full Stack Engineer at Quatt
                 </span>
+                <br />
+                <Link
+                  href="https://www.linkedin.com/in/craigtsdev/"
+                  aria-label="Go to LinkedIn of Craig Smith"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-sm text-gray-600 underline underline-offset-4 hover:text-gray-800"
+                >
+                  Go to their LinkedIn
+                </Link>
               </div>
             </div>
             <p>
@@ -43,57 +79,42 @@ export default function Testimonials() {
               to do so again.”
             </p>
           </div>
-          <div className="flex flex-col gap-4">
-            <div className="flex items-center gap-6">
-              <Image
-                src={Omar}
-                alt="Photo of Omar Reiss"
-                width={128}
-                height={128}
-                className="rounded-full"
-              />
-              <div>
-                <span className="font-heading text-3xl">Omar Reiss</span>
-                <br />
-                <span className="text-xl font-light">CTO at Yoast</span>
+          {rotatingTestimonials.map((testimonial) => (
+            <div
+              key={testimonial.name}
+              className="mt-10 flex flex-col gap-4 md:mt-0"
+            >
+              <div className="flex items-center gap-6">
+                <Image
+                  src={testimonial.image}
+                  alt={testimonial.alt}
+                  width={128}
+                  height={128}
+                  className="rounded-full"
+                />
+                <div>
+                  <span className="font-heading text-3xl">
+                    {testimonial.name}
+                  </span>
+                  <br />
+                  <span className="text-xl font-light">
+                    {testimonial.title}
+                  </span>
+                  <br />
+                  <Link
+                    href={testimonial.linkedin}
+                    aria-label={`Go to LinkedIn of ${testimonial.name}`}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-sm text-gray-600 underline underline-offset-4 hover:text-gray-800"
+                  >
+                    Go to their LinkedIn
+                  </Link>
+                </div>
               </div>
+              <p>“{testimonial.quote}”</p>
             </div>
-            <p>
-              “Anton is a multi-talent, a really smart, enthusiastic and curious
-              person. He is truly full stack as a software engineer and a very
-              fast learner. He’s also proven himself to be a great mentor and
-              teacher to his fellow engineers. I’ve deeply enjoyed working with
-              him and have learned a lot in doing so. Don’t miss out on a chance
-              to work with him!”
-            </p>
-          </div>
-          <div className="mt-10 flex flex-col gap-4 md:mt-0">
-            <div className="flex items-center gap-6">
-              <Image
-                src={Niels}
-                alt="Photo of Niels de Blauw"
-                width={128}
-                height={128}
-                className="rounded-full"
-              />
-              <div>
-                <span className="font-heading text-3xl">Niels de Blauw</span>
-                <br />
-                <span className="text-xl font-light">
-                  Lead Developer at Level Level
-                </span>
-              </div>
-            </div>
-            <p>
-              “Anton is able to solve very complex problems with creative yet
-              sensible solutions. He picked up multiple roles from building the
-              user interface, adding pricing algorithms and integrating these
-              new features into the production process of the manufacturer. He
-              has extensive knowledge of a lot of aspects in web development. He
-              knows how to clearly communicate his thoughts and when to ask
-              questions to learn more.”
-            </p>
-          </div>
+          ))}
         </div>
       </BaseBlock>
     </Container>
