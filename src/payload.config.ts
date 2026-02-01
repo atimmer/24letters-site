08277eed5001db2a78b709000194579a4a05d05d
Anton Timmermans <email@atimmer.com>
2026-02-01 15:24:41 +0100

    fix: add .js extensions in payload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,8 +5,8 @@ import { buildConfig } from "payload";
 import { fileURLToPath } from "url";
 import sharp from "sharp";
 
-import { Users } from "./collections/Users";
-import { Media } from "./collections/Media";
+import { Users } from "./collections/Users.js";
+import { Media } from "./collections/Media.js";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
