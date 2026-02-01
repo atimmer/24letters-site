09a735bf00c29fc29dc341a6cb1ade0df9cbad26
Anton Timmermans <email@atimmer.com>
2026-02-01 15:49:06 +0100

    chore: add initial migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260201_144634 from './20260201_144634';
+
+export const migrations = [
+  {
+    up: migration_20260201_144634.up,
+    down: migration_20260201_144634.down,
+    name: '20260201_144634'
+  },
+];
